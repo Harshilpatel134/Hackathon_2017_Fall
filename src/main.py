4595baacb18626efc45dbed58fd1eaff662a31df
Yong Zheng <yongzheng0809@gmail.com>
2017-11-11 13:57:06 -0600

    Add statistics page

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ import docker_monitor
 from flask import Flask, render_template, request, redirect, jsonify, url_for
 from pymongo import MongoClient
 from flask_apscheduler import APScheduler
+import plotly.graph_objs as go
+import plotly
 
 app = Flask(__name__)
 mongo_client = MongoClient('mongodb://mongo_user:mongo_pass@ds257485.mlab.com:57485/hackathon2017')
@@ -16,6 +18,35 @@ collection = db['hackathon']
 def index():
     record = collection.find().sort([('timestamp', -1)]).limit(1)[0]
     return render_template('index.html', record=record)
+
+@app.route("/statistics")
+def statistics():
+    timestamp = []
+    active_containers = []
+    all_up = 0
+    all_down = 0
+    partial = 0
+    for row in collection.find():
+        count = 0
+        for i in row['result']:
+            if row['result'][i]['state'] != 'exited':
+                count += 1
+        timestamp.append(row['timestamp'])
+        active_containers.append(count)
+    for row in collection.find():
+        row = [row['result'][i]['state'] for i in row['result']]
+        if len(set(row)) == 1:
+            if 'exited' in row:
+                all_down += 1
+            else:
+                all_up +=1
+        else:
+            partial +=1
+    data = [go.Scatter(x=timestamp, y=active_containers)]
+    fig = go.Figure(data=data)
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('statistics.html', graphJSON=graphJSON, all_up=all_up, all_down=all_down, partial=partial)
+
 
 @app.route("/info/<containder_id>")
 def get_info(containder_id):
@@ -28,6 +59,11 @@ def get_info(containder_id):
 def refresh():
     docker_monitor.run()
     return redirect("/")
+
+@app.route("/refresh_stat", methods=['POST'])
+def refresh_stat():
+    docker_monitor.run()
+    return redirect("/statistics")
 
 @app.route("/restart", methods=['POST'])
 def restart():
@@ -69,5 +105,5 @@ if __name__ == '__main__':
     scheduler.start()
     # Initial run when app starts
     docker_monitor.run()
-
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
